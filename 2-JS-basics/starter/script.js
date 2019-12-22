23a8e1e5f43e45f087d6fe41fabcc57eed06cb05
tubidge <tubidge@gmail.com>
2019-12-22 12:22:07 -0700

    coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,13 +1,41 @@
-var firstName = 'John';
-console.log(firstName);
+/*******************
+* Variables and Data Types
+*******************/
 
-var lastName = 'Smith';
+// var firstName = 'John';
+// console.log(firstName);
+
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullAge = true;
+
+// var job;
+// console.log(job);
+
+// job = 'Teacher';
+// console.log(job);
+
+// // Variable naming rules
+// var _3years = 3;
+// var johnMark = 'John and Mark';
+// var if = 23;
+
+/*******************
+* Variables Mutation and Type Coercion
+*******************/
+
+var firstName = 'John';
 var age = 28;
 
-var fullAge = true;
+console.log(firstName + ' ' + age);
 
-var job;
-console.log(job);
-
+var job, isMarried;
 job = 'Teacher';
-console.log(job);
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old '
+    + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
