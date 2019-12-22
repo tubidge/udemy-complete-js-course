f214fd1cf0bc16b9e50eaeb7a564153f6750d23b
tubidge <tubidge@gmail.com>
2019-12-22 12:09:40 -0700

    data types

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,2 +1,13 @@
 var firstName = 'John';
 console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
