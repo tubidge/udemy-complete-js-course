b5e459e0f3370979b05813d1deff4b50bb6aaac6
tubidge <tubidge@gmail.com>
2020-01-07 19:29:53 -0700

    starting first project

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,14 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+// Score keeping
+var scores = [0, 0];
+var roundScore = 0;
+var activePlayer = 0;
+
+var dice = Math.floor(Math.random() * 6) + 1;
+console.log('Dice roll: ' + dice);
+
+document.querySelector('#score-0').textContent = dice;
+document.getElementById('score-1').textContent = dice;
