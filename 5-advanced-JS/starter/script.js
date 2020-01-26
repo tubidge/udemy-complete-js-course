f7b97387c676a32f8cd5381837abaa622169d6de
tubidge <tubidge@gmail.com>
2020-01-26 16:57:21 -0700

    prototype inheritance

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -12,4 +12,18 @@ var Person = function (name, yearOfBirth, job) {
     this.job = job;
 };
 
+Person.prototype.calcAge = function () {
+    console.log(this.name + ' ' + this.lastName + ' Age: ' + (2020 - this.yearOfBirth));
+};
+
+Person.prototype.lastName = 'Smith';
+
 var mark = new Person('Mark', 1988, 'Fighter');
+
+
+var jane = new Person('Jane', 1969, 'Artist');
+var phil = new Person('Phil', 1909, 'Retired');
+
+mark.calcAge();
+jane.calcAge();
+phil.calcAge();
