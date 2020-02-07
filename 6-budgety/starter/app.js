7be662f314b427ab8588ba70f74756b3009dc833
tubidge <tubidge@gmail.com>
2020-02-06 18:42:20 -0700

    capturing user input

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,30 +1,40 @@
 var budgetController = (function () {
-    var x = 23;
-    var add = function (a) {
-        return x + a;
-    };
 
-    return {
-        publicTest: function (b) {
-            return (add(b));
-        }
-    };
+
 })();
+
 
 
 var uiController = (function () {
 
+    return {
+        getinput: function () {
 
+        }
+    }
 })();
 
 
-var controller = (function (budgeCtrl, uiCtrl) {
-    var z = budgeCtrl.publicTest(7);
 
-    return {
-        publicMethod: function () {
-            console.log(z);
+var appController = (function (budgetCtrl, uiCtrl) {
+
+    var ctrlAddItem = function () {
+        // 1. Capture field input data
+        // 2. Add item to budget controller
+        // 3. Add item to UI
+        // 4. Calculate budget
+        // 5. Display new budget in UI
+        console.log('it works');
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
         }
-    }
+
+
+    })
 
 })(budgetController, uiController);
